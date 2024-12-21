dd1b13ed6f94aef224fff85138faa968d4483f8a
Hammed Adigun <adigunadunfe@gmail.com>
2024-12-21 07:48:51 +0000

    Time: 6 ms (29.76%), Space: 44.9 MB (37.22%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,17 +1,17 @@
 class Solution {
     public int romanToInt(String s) {
-        Map<Character, Integer> romanMap = new LinkedHashMap<>();
+        Map<Character, Integer> values = new LinkedHashMap<>();
         
         // Populate the map in descending order
-        romanMap.put('M', 1000);
-        romanMap.put('D', 500);
-        romanMap.put('C', 100);
-        romanMap.put('L', 50);
-        romanMap.put('X', 10);
-        romanMap.put('V', 5);
-        romanMap.put('I', 1);
+        values.put('M', 1000);
+        values.put('D', 500);
+        values.put('C', 100);
+        values.put('L', 50);
+        values.put('X', 10);
+        values.put('V', 5);
+        values.put('I', 1);
 
-        int result = 0, n = s.trim().length();
+        /*int result = 0, n = s.trim().length();
         for (int i = 0; i < n; i++) {
             int curValue = romanMap.get(s.charAt(i));
             int nextValue = i + 1 < n ? romanMap.get(s.charAt(i + 1)) : 0;
@@ -21,6 +21,29 @@ class Solution {
                 result -= curValue;
             }
         }
+        return result;*/
+
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int currentVal = values.get(s.charAt(i));
+
+            // Check the next character if it exists
+            if (i + 1 < s.length()) {
+                int nextVal = values.get(s.charAt(i + 1));
+
+                // If current is less than next, subtract current
+                if (currentVal < nextVal) {
+                    result -= currentVal;
+                } else {
+                    // Otherwise, add current
+                    result += currentVal;
+                }
+            } else {
+                // Last character, just add
+                result += currentVal;
+            }
+        }
+
         return result;
     }
 }
