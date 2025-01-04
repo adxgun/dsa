66bac7e250e0659cd38def185e994b77627e8b77
Hammed Adigun <adigunadunfe@gmail.com>
2025-01-04 19:05:11 +0000

    Time: 1 ms (83.85%), Space: 41.9 MB (52.07%) - LeetHub

---
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder sb = new StringBuilder();
+        int i = 0, j = 0, l1 = word1.length(), l2 = word2.length();
+        while(i < l1 || j < l2) {
+            if (i < l1) {
+                sb.append(word1.charAt(i));
+                i++;
+            }
+
+            if (j < l2) {
+                sb.append(word2.charAt(j));
+                j++;
+            }
+        }
+        
+        return sb.toString();
+    }
+}
