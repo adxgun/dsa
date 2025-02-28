0fafe702eb4ed910dcd53a668c011d42566b6ab0
Hammed Adigun <adigunadunfe@gmail.com>
2025-02-28 16:39:21 +0000

    Time: 1 ms (97.52%), Space: 45.2 MB (96.85%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -1,13 +1,9 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        if (n == 0) {
-            return true;
-        }
-
         for (int i = 0; i < flowerbed.length; i++) {
             if (flowerbed[i] == 0) {
-                boolean emptyLeft = (i == 0) || flowerbed[i - 1] == 0;
-                boolean emptyRight = (i == flowerbed.length - 1) || flowerbed[i + 1] == 0;
+                boolean emptyLeft = (i == 0) || (flowerbed[i - 1] == 0);
+                boolean emptyRight = (i == flowerbed.length - 1) || (flowerbed[i + 1] == 0);
 
                 if (emptyLeft && emptyRight) {
                     flowerbed[i] = 1;
@@ -19,6 +15,7 @@ class Solution {
                 }
             }
         }
+
         return n <= 0;
     }
 }
