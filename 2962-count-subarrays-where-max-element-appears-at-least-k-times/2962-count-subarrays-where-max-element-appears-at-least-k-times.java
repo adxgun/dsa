a1baa56829af78836fd4e208a10f24e45af5dc38
Hammed Adigun <adigunadunfe@gmail.com>
2026-06-09 15:11:20 +0100

    Time: 5 ms (87.89%), Space: 109.3 MB (79.75%) - LeetHub

--- 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.java
+++ 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.java
@@ -1,5 +1,5 @@
 class Solution {
-    public long countSubarrays(int[] nums, int k) {
+    public long countSubarraysA(int[] nums, int k) {
         int maxElem = -1, left = 0, n = nums.length;
         long len = 0;
         for (int x : nums) maxElem = Math.max(maxElem, x);
@@ -16,5 +16,28 @@ class Solution {
             len += left;
         }
         return len;
+    }
+
+    public long countSubarrays(int[] nums, int k) {
+        int maxElem = 0;
+        for (int x : nums) maxElem = Math.max(maxElem, x);
+
+        long count = 0;
+        int left = 0, maxCount = 0;
+        int n = nums.length;
+
+        for (int right = 0; right < n; right++) {
+            if (nums[right] == maxElem) maxCount++;
+
+            // Shrink from the left while we still have at least k maxes
+            while (maxCount >= k) {
+                if (nums[left] == maxElem) maxCount--;
+                left++;
+            }
+
+            // Now [0..left-1] are valid start positions for subarrays ending at `right`
+            count += left;
+        }
+        return count;
     }
 }
