8ff345d86351dd60230c17dbc252fe9f3157fb29
Hammed Adigun <adigunadunfe@gmail.com>
2026-06-09 15:10:02 +0100

    Time: 97 ms (5.22%), Space: 123.8 MB (5.01%) - LeetHub

--- 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.java
+++ 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.java
@@ -1,24 +1,20 @@
 class Solution {
     public long countSubarrays(int[] nums, int k) {
-        int maxElem = 0;
+        int maxElem = -1, left = 0, n = nums.length;
+        long len = 0;
         for (int x : nums) maxElem = Math.max(maxElem, x);
 
-        long count = 0;
-        int left = 0, maxCount = 0;
-        int n = nums.length;
-
+        var freq = new HashMap<Integer, Integer>();
         for (int right = 0; right < n; right++) {
-            if (nums[right] == maxElem) maxCount++;
+            freq.merge(nums[right], 1, Integer::sum);
 
-            // Shrink from the left while we still have at least k maxes
-            while (maxCount >= k) {
-                if (nums[left] == maxElem) maxCount--;
+            while(freq.get(maxElem) != null && freq.get(maxElem) >= k) {
+                if (freq.merge(nums[left], -1, Integer::sum) == 0) freq.remove(nums[left]);
                 left++;
             }
 
-            // Now [0..left-1] are valid start positions for subarrays ending at `right`
-            count += left;
+            len += left;
         }
-        return count;
+        return len;
     }
 }
