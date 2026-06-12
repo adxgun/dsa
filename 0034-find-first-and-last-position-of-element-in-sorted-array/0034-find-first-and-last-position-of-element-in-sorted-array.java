2c150bfcd3673b5969b1c714e19e7b5c66a35be3
Hammed Adigun <adigunadunfe@gmail.com>
2026-06-12 11:13:49 +0100

    Time: 0 ms (100%), Space: 48.3 MB (13.19%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,34 +1,42 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] result = new int[]{-1, -1};
-        if (nums == null || nums.length == 0) {
-            return result;
-        }
+        if (nums.length == 0) return new int[]{-1, -1};
 
-        
-        result[0] = searchRange(nums, target, true);
-        result[1] = searchRange(nums, target, false);
-        return result;
+        return new int[]{
+            findLeftBoundary(nums, target),
+            findRightBoundary(nums, target),
+        };
     }
 
-    private int searchRange(int[] nums, int target, boolean first) {
-        int left = 0, right = nums.length - 1;
-        int idx = -1;
-        while(left <= right) {
-            int mid = (left + right) / 2;
+    private int findLeftBoundary(int[] nums, int target) {
+        int left = 0, right = nums.length - 1, bound = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
-                idx = mid;
-                if (first) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
+                bound = mid;
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
             } else if (nums[mid] > target) {
                 right = mid - 1;
-            } else {
-                left = mid + 1;
             }
         }
-        return idx;
+        return bound;
+    }
+
+    private int findRightBoundary(int[] nums, int target) {
+        int left = 0, right = nums.length - 1, bound = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                bound = mid;
+                left = mid + 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            }
+        }
+        return bound;
     }
 }
