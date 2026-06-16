7e89a62eb86ecf3a1255d64f91178e4c86b4b4b9
Hammed Adigun <adigunadunfe@gmail.com>
2026-06-16 15:00:00 +0100

    Time: 8 ms (15.45%), Space: 48.1 MB (5.34%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int missingNumber(int[] nums) {
+    public int missingNumberA(int[] nums) {
         var map = new HashMap<Integer, Integer>();
         for (int i = 0; i < nums.length; i++) map.put(nums[i], i + 1);
 
@@ -8,5 +8,17 @@ class Solution {
         }
 
         return 0;
+    }
+
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        int lo = 0, hi = nums.length - 1;
+
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] == mid) lo = mid + 1;
+            else hi = mid - 1;
+        }
+        return lo;
     }
 }
