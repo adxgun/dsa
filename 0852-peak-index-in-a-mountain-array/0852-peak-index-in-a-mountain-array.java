9c3e75c5cff97ba559def6a3183ad66c2ee4e59b
Hammed Adigun <adigunadunfe@gmail.com>
2026-07-05 17:01:48 +0100

    Time: 0 ms (100%), Space: 80.1 MB (76.96%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -1,12 +1,12 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        int max = 0, idx = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
-                idx = i;
-            }
+        int n = arr.length, lo = 0, hi = n;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (mid + 1 < n && arr[mid] < arr[mid + 1]) {
+                lo = mid + 1;
+            } else hi = mid;
         }
-        return idx;
+        return lo;
     }
 }
