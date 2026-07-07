20877bbb595e4f4b39aff1118dab83ede0852492
Hammed Adigun <adigunadunfe@gmail.com>
2026-07-07 12:47:06 +0100

    Time: 5 ms (72.06%), Space: 44.1 MB (73.53%) - LeetHub

--- 1099-two-sum-less-than-k/1099-two-sum-less-than-k.java
+++ 1099-two-sum-less-than-k/1099-two-sum-less-than-k.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int twoSumLessThanK(int[] nums, int k) {
+    public int twoSumLessThanK_A(int[] nums, int k) {
         int i = 0, j = nums.length - 1, max = -1;
         Arrays.sort(nums);
         
@@ -12,5 +12,27 @@ class Solution {
             }
         }
         return max;
+    }
+
+    public int twoSumLessThanK(int[] nums, int k) {
+        Arrays.sort(nums);
+        int n = nums.length, max = -1;
+        for (int i = 0; i < n - 1; i++) {
+            int target = k - nums[i];
+            int idx = lowerBound(nums, i + 1, n - 1, target) - 1;
+            if (idx > i) {
+                max = Math.max(max, nums[i] + nums[idx]);
+            }
+        }
+        return max;
+    }
+
+    private int lowerBound(int[] nums, int lo, int hi, int target) {
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] < target) lo = mid + 1;
+            else hi = mid - 1;
+        }
+        return lo;
     }
 }
