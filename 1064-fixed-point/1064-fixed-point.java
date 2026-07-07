b43b746dd464a33a7c1b7b47a5dc74dc8ad8c39e
Hammed Adigun <adigunadunfe@gmail.com>
2026-07-07 12:18:11 +0100

    Time: 0 ms (100%), Space: 46.4 MB (92.65%) - LeetHub

---
+++ 1064-fixed-point/1064-fixed-point.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int fixedPoint(int[] arr) {
+        int lo = 0, hi = arr.length - 1, answer = -1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (arr[mid] == mid) {
+                answer = mid;
+                hi = mid - 1;
+            }
+            else if (arr[mid] < mid) lo = mid + 1;
+            else hi = mid - 1;
+        }
+        return answer;
+    }
+}
