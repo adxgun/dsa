11a2ca2a04627ec5e3a9c04cb5efe0776e19eaaf
Hammed Adigun <adigunadunfe@gmail.com>
2026-07-07 10:24:26 +0100

    Time: 11 ms (88.81%), Space: 47.4 MB (23.5%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,5 +1,5 @@
 class Solution {
-    public String frequencySort(String s) {
+    public String frequencySortA(String s) {
         var map = new HashMap<Character, Integer>();
         for (char c : s.toCharArray()) {
             map.merge(c, 1, Integer::sum);
@@ -19,5 +19,29 @@ class Solution {
             sb.append(c);
             count--;
         }
+    }
+
+    public String frequencySort(String s) {
+        int n = s.length();
+        Map<Character, Integer> count = new HashMap<>();
+        for (char c : s.toCharArray()) count.merge(c, 1, Integer::sum);
+
+        // buckets[f] = list of characters occurring exactly f times
+        List<Character>[] buckets = new List[n + 1];
+        for (var entry : count.entrySet()) {
+            int f = entry.getValue();
+            if (buckets[f] == null) buckets[f] = new ArrayList<>();
+            buckets[f].add(entry.getKey());
+        }
+
+        StringBuilder sb = new StringBuilder();
+        // read from highest frequency down
+        for (int f = n; f >= 1; f--) {
+            if (buckets[f] == null) continue;
+            for (char c : buckets[f]) {
+                for (int i = 0; i < f; i++) sb.append(c);   // append c, f times
+            }
+        }
+        return sb.toString();
     }
 }
