d9bb04062d0d1754c1a9b1e44c124f393fb86cd1
Hammed Adigun <adigunadunfe@gmail.com>
2026-07-08 17:29:07 +0100

    Time: 13 ms (84.39%), Space: 54.5 MB (14.87%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.java
+++ 1268-search-suggestions-system/1268-search-suggestions-system.java
@@ -1,20 +1,32 @@
 class Solution {
     public List<List<String>> suggestedProducts(String[] products, String searchWord) {
         Arrays.sort(products);
-        int N = searchWord.length();
         List<List<String>> res = new ArrayList<>();
-        for (int i = 0; i < N; i++) {
+        String prefix = "";
+
+        for (char c : searchWord.toCharArray()) {
             List<String> matches = new ArrayList<>();
-            String sub = searchWord.substring(0, i + 1);
-            for (String product : products) {
-                if (product.startsWith(sub) && matches.size() < 3) {
-                    matches.add(product);
-                }
+            prefix += c;
+
+            int start = lowerBound(products, prefix);
+            for (int i = start; i < Math.min(start + 3, products.length); i++) {
+                if (products[i].startsWith(prefix)) matches.add(products[i]);
+                else break;
             }
 
             res.add(matches);
         }
 
         return res;
+    }
+
+    private int lowerBound(String[] products, String prefix) {
+        int lo = 0, hi = products.length - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (products[mid].compareTo(prefix) < 0) lo = mid + 1;
+            else hi = mid - 1;
+        }
+        return lo;
     }
 }
