be5923bc617ce4ba4bbbf6deab392be69b8fa024
Hammed Adigun <adigunadunfe@gmail.com>
2026-07-09 11:33:19 +0100

    Time: 8 ms (20.49%), Space: 46.5 MB (20.16%) - LeetHub

--- 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.java
+++ 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
+    public int findTheDistanceValueA(int[] arr1, int[] arr2, int d) {
         int count = 0;
         for (int x : arr1) {
             boolean all = true;
@@ -13,5 +13,28 @@ class Solution {
             if (all) count += 1;
         }
         return count;
+    }
+
+    public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
+        java.util.Arrays.sort(arr2);
+        int count = 0;
+        for (int x : arr1) {
+            // find first arr2 element >= x - d; if it's <= x + d, x is within d of something
+            int idx = lowerBound(arr2, x - d);
+            if (idx == arr2.length || arr2[idx] > x + d) {
+                count++;   // nothing in [x-d, x+d] -> x qualifies
+            }
+        }
+        return count;
+    }
+
+    private int lowerBound(int[] arr, int target) {
+        int lo = 0, hi = arr.length;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (arr[mid] < target) lo = mid + 1;
+            else hi = mid;
+        }
+        return lo;
     }
 }
