a4209a34d6fb9f217779ea5376bb52d73425bfa2
Hammed Adigun <adigunadunfe@gmail.com>
2026-07-10 12:12:21 +0100

    Time: 75 ms (5.98%), Space: 46.6 MB (9.58%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int specialArray(int[] nums) {
+    public int specialArrayA(int[] nums) {
         int n = nums.length;
         for (int i = 0; i < 1000; i++) {
             int count = 0;
@@ -8,5 +8,27 @@ class Solution {
             if (count == i) return count;
         }
         return -1;
+    }
+
+    public int specialArray(int[] nums) {
+        int n = nums.length;
+        int N = 1000;
+        Arrays.sort(nums);
+        for (int i = 0; i < N; i++) {
+            int ub = upperBound(nums, i);
+            System.out.println("ub -> " + ub);
+            if (n - ub == i) return i;
+        }
+        return -1;
+    }
+
+    private int upperBound(int[] arr, int val) {
+        int lo = 0, hi = arr.length - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (arr[mid] < val) lo = mid + 1;
+            else hi = mid - 1;
+        }
+        return lo;
     }
 }
