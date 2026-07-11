68af4046fff8d63238ca03c2b1948e1472cac720
Hammed Adigun <adigunadunfe@gmail.com>
2026-07-11 15:49:20 +0100

    Time: 84 ms (5.17%), Space: 103.8 MB (5.01%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int maxFrequency(int[] nums, int k) {
+    public int maxFrequency_SW(int[] nums, int k) {
         Arrays.sort(nums);
         long windowSum = 0;
         int left = 0, maxFreq = 0;
@@ -17,7 +17,32 @@ class Solution {
         return maxFreq;
     }
 
-    public int maxFrequencyA(int[] nums, int k) {
-        return 0;
+    public int maxFrequency(int[] nums, int k) {
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        long[] prefix = new long[n + 1];
+        for (int i = 0; i < n; i++) prefix[i + 1] = prefix[i] + nums[i];
+
+        int maxFreq = 0;
+        for (int right = 0; right < n; right++) {
+
+            int lo = 0, hi = right;
+            while (lo < hi) {
+                int mid = lo + (hi - lo) / 2;
+                if (cost(prefix, mid, right, nums[right]) <= k) {
+                    hi = mid;
+                } else lo = mid + 1;
+            }
+
+            maxFreq = Math.max(maxFreq, right - lo + 1);
+        }
+        return maxFreq;
+    }
+
+    private long cost(long[] prefix, int left, int right, int target) {
+        int size = right - left + 1;
+        long windowSum = prefix[right + 1] - prefix[left];
+        return (long) target * size - windowSum;
     }
 }
