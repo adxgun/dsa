e6e9ac9ff85b0206ba500a37a32cd5a4e9f69c74
Hammed Adigun <adigunadunfe@gmail.com>
2026-07-11 17:14:22 +0100

    Time: 21 ms (8.74%), Space: 84.9 MB (95.07%) - LeetHub

--- 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.java
+++ 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int maxDistance(int[] nums1, int[] nums2) {
+    public int maxDistanceA(int[] nums1, int[] nums2) {
         int dist = 0, n = nums1.length;
         for (int i = 0; i < n; i++) {
             // return the first idx of elem in nums2 >= x
@@ -17,5 +17,35 @@ class Solution {
             else lo = mid + 1;
         }
         return lo - 1;
+    }
+
+    public int maxDistance(int[] nums1, int[] nums2) {
+        int n1 = nums1.length, n2 = nums2.length;
+        int dist = 0;
+
+        for (int i = 0; i < n1; i++) {
+            // largest j in nums2 with nums2[j] >= nums1[i] (nums2 is non-increasing)
+            int j = lastAtLeast(nums2, i, nums1[i]);
+            if (j >= i) {
+                dist = Math.max(dist, j - i);
+            }
+        }
+        return dist;
+    }
+
+    // in a non-increasing array, find the largest index j (>= startFrom) with nums[j] >= val;
+    // returns -1 if none
+    private int lastAtLeast(int[] nums, int startFrom, int val) {
+        int lo = startFrom, hi = nums.length - 1, result = -1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] >= val) {
+                result = mid;      // this qualifies; look further right for a larger index
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;      // too small; qualifying values are to the left
+            }
+        }
+        return result;
     }
 }
