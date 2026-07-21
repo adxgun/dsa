2d08d1a98c935aef5602e2e38efb7b29b51538e2
Hammed Adigun <adigunadunfe@gmail.com>
2026-07-21 14:21:41 +0100

    Time: 15 ms (73.41%), Space: 70.1 MB (13.5%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -18,4 +18,24 @@ class Solution {
 
         return left != null ? left : right;
     }
+
+    public TreeNode lowestCommonAncestorA(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode cur = root;
+        while (cur != null) {
+            if (p.val < cur.val && q.val < cur.val) cur = cur.left;
+            else if (p.val > cur.val && q.val > cur.val) cur = cur.right;
+            else return cur;
+        }
+        return null;
+    }
+
+    public TreeNode lowestCommonAncestorB(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode cur = root;
+        while (cur != null) {
+            if (p.val < cur.val && q.val < cur.val)      cur = cur.left;   // both smaller → go left
+            else if (p.val > cur.val && q.val > cur.val) cur = cur.right;  // both larger → go right
+            else return cur;   // they straddle cur (or cur is one of them) → split point
+        }
+        return null;
+    }
 }
