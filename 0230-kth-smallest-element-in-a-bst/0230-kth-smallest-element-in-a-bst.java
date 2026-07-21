34a4f4646d2800c0354a0ef2a70039138a701cbc
Hammed Adigun <adigunadunfe@gmail.com>
2026-07-21 13:23:19 +0100

    Time: 0 ms (100%), Space: 46.7 MB (69.33%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -16,20 +16,24 @@
 class Solution {
     
     PriorityQueue<Integer> q = new PriorityQueue<>(Collections.reverseOrder());
-    int k = 0;
+    int k = 0, inOrderLen = 0, kthSmallest = -1;
 
     public int kthSmallest(TreeNode root, int k) {
         this.k = k;
         dfs(root);
-        return q.poll();
+        return kthSmallest;
     }
 
     private void dfs(TreeNode root) {
         if (root == null) return;
 
+        if (kthSmallest != -1) return;
+
         dfs(root.left);
+        inOrderLen++;
+        if (inOrderLen == k) kthSmallest = root.val;
         dfs(root.right);
-        q.offer(root.val);
-        if (q.size() > k) q.poll();
+        // q.offer(root.val);
+        // if (q.size() > k) q.poll();
     }
 }
