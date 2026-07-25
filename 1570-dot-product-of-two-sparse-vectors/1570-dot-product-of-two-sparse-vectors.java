b5bd05b1882b018e78402be8ac8d61d345381baa
Hammed Adigun <adigunadunfe@gmail.com>
2026-07-25 11:16:43 +0100

    Time: 9 ms (63.78%), Space: 99.8 MB (36.22%) - LeetHub

--- 1570-dot-product-of-two-sparse-vectors/1570-dot-product-of-two-sparse-vectors.java
+++ 1570-dot-product-of-two-sparse-vectors/1570-dot-product-of-two-sparse-vectors.java
@@ -1,22 +1,25 @@
 class SparseVector {
     
-    private int[] nums;
-    SparseVector(int[] nums) {
-        this.nums = nums;
+    public Map<Integer, Integer> nonZeros;
+    SparseVector(int[] arr) {
+        this.nonZeros = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) nonZeros.put(i, arr[i]);
+        }
     }
     
 	// Return the dotProduct of two sparse vectors
     public int dotProduct(SparseVector vec) {
         int sum = 0;
-        int[] left = vec.getNums();
-        for (int i = 0; i < nums.length; i++) {
-            sum += left[i] * nums[i];
+
+        Map<Integer, Integer> smaller = this.nonZeros.size() <= vec.nonZeros.size() ? this.nonZeros : vec.nonZeros;
+
+        Map<Integer, Integer> other = this.nonZeros == smaller ? vec.nonZeros : this.nonZeros;
+        for (Map.Entry<Integer, Integer> entry : smaller.entrySet()) {
+            Integer otherValue = other.get(entry.getKey());
+            if (otherValue != null) sum += otherValue * entry.getValue();
         }
         return sum;
-    }
-
-    public int[] getNums() {
-        return this.nums;
     }
 }
 
