1f6d06fd4ad7d216137558ef1dbe8a255d5f0c21
Hammed Adigun <adigunadunfe@gmail.com>
2026-07-27 21:17:56 +0100

    Time: 11 ms (89.79%), Space: 52.8 MB (84.83%) - LeetHub

--- 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.java
+++ 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.java
@@ -15,11 +15,14 @@
  */
 class Solution {
 
+    private Map<Integer, Integer> treeCount = new HashMap<>();
     private Map<String, Integer> count = new HashMap<>();
     private List<TreeNode> result = new ArrayList<>();
+    private Map<String, Integer> treeId = new HashMap<>();
+    private int idCounter = 0;
     private static String NULL = "#";
     
-    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+    public List<TreeNode> findDuplicateSubtreesA(TreeNode root) {
         dfs(root);
         return result;    
     }
@@ -35,5 +38,24 @@ class Solution {
 
         if (c == 2) result.add(root);
         return triple;
+    }
+
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        getId(root);
+        return result;
+    }
+
+    private int getId(TreeNode root) {
+        if (root == null) return 0;
+
+        int left = getId(root.left);
+        int right = getId(root.right);
+
+        String triple = root.val + "," + left + "," + right;
+        int id = treeId.computeIfAbsent(triple, k -> ++idCounter);
+        int val = treeCount.merge(id, 1, Integer::sum);
+
+        if (val == 2) result.add(root);
+        return id;
     }
 }
