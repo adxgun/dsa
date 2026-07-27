eed84e9095dd7ef4d47fa9c7e753c3135958006e
Hammed Adigun <adigunadunfe@gmail.com>
2026-07-27 21:33:00 +0100

    Time: 3 ms (73.47%), Space: 47.5 MB (21.13%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -16,21 +16,32 @@
 
 class Solution {
     private boolean hasSum = false;
-    public boolean findTarget(TreeNode root, int k) {
+    public boolean findTargetA(TreeNode root, int k) {
         Set<Integer> set = new HashSet<>();
         dfs(root, set, k);
         return hasSum;
     }
 
-    private void dfs(TreeNode root, Set<Integer> set, int target) {
+    private void dfsA(TreeNode root, Set<Integer> set, int target) {
         if (root == null) return;
 
         if (hasSum) return;
 
-        dfs(root.left, set, target);
+        dfsA(root.left, set, target);
         int compliment = target - root.val;
         if (set.contains(compliment)) hasSum = true;
         set.add(root.val);
-        dfs(root.right, set, target);
+        dfsA(root.right, set, target);
+    }
+
+    public boolean findTarget(TreeNode root, int k) {
+        return dfs(root, new HashSet<>(), k);
+    }
+
+    private boolean dfs(TreeNode node, Set<Integer> set, int target) {
+        if (node == null) return false;
+        if (set.contains(target - node.val)) return true;
+        set.add(node.val);
+        return dfs(node.left, set, target) || dfs(node.right, set, target);
     }
 }
