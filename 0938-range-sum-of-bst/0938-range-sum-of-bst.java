7d578e0998fb0fb4354a1d0a64e46e5fa3db9e06
Hammed Adigun <adigunadunfe@gmail.com>
2026-07-28 16:10:47 +0100

    Time: 0 ms (100%), Space: 54.2 MB (13.08%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.java
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.java
@@ -15,7 +15,7 @@
  */
 class Solution {
     int res = 0, low = 0, high = 0;
-    public int rangeSumBST(TreeNode root, int low, int high) {
+    public int rangeSumBST1(TreeNode root, int low, int high) {
         this.low = low;
         this.high = high;
         dfs(root);
@@ -28,5 +28,24 @@ class Solution {
         dfs(root.left);
         if (root.val >= low && root.val <= high) res += root.val;
         dfs(root.right);
+    }
+
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        this.low = low;
+        this.high = high;
+        traverse(root);
+        return res;
+    }
+
+    private void traverse(TreeNode root) {
+        if (root == null) return;
+
+        if (root.val < low) traverse(root.right);
+        else if (root.val > high) traverse(root.left);
+        else {
+            res += root.val;
+            traverse(root.left);
+            traverse(root.right);
+        }
     }
 }
