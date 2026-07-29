b3ef4e4b8e4e9d31d9d91df48b1b68fb9139a3c9
Hammed Adigun <adigunadunfe@gmail.com>
2026-07-29 22:31:27 +0100

    Time: 0 ms (100%), Space: 46.9 MB (34.78%) - LeetHub

--- 1430-check-if-a-string-is-a-valid-sequence-from-root-to-leaves-path-in-a-binary-tree/1430-check-if-a-string-is-a-valid-sequence-from-root-to-leaves-path-in-a-binary-tree.java
+++ 1430-check-if-a-string-is-a-valid-sequence-from-root-to-leaves-path-in-a-binary-tree/1430-check-if-a-string-is-a-valid-sequence-from-root-to-leaves-path-in-a-binary-tree.java
@@ -19,10 +19,15 @@ class Solution {
     private List<Integer> path = new ArrayList<>();
     
     public boolean isValidSequence(TreeNode root, int[] arr) {
-        List<Integer> comp = new ArrayList<>();
-        for (int x : arr) comp.add(x);
-        dfs(root, comp);
-        return isValid;
+        return isValidSequence(root, arr, 0);
+    }
+
+    private boolean isValidSequence(TreeNode root, int[] arr, int index) {
+        if (root == null || index >= arr.length || root.val != arr[index]) return false;
+
+        if (root.left == null && root.right == null) return index == arr.length - 1;
+        return isValidSequence(root.left, arr, index + 1) ||
+            isValidSequence(root.right, arr, index + 1);
     }
 
     private void dfs(TreeNode root, List<Integer> arr) {
