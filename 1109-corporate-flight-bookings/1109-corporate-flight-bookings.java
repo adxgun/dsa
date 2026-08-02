b65b7de38fcab49a7a84c6a619383eee8208eaf1
Hammed Adigun <adigunadunfe@gmail.com>
2026-08-02 18:21:31 +0100

    Time: 3 ms (98.3%), Space: 84.9 MB (32.94%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.java
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.java
@@ -1,9 +1,7 @@
 class Solution {
     public int[] corpFlightBookings(int[][] bookings, int n) {
         int[] flights = new int[n];
-        for (int i = 1; i < n; i++) {
-            flights[i] = flights[i] - flights[i-1];
-        }
+        
 
         for (int[] booking : bookings) {
             int start = booking[0] - 1;
