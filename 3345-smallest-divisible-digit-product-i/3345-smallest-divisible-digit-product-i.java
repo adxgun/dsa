3d89cf5ed82e14e29ccceb33a0297c7caae6f309
Hammed Adigun <adigunadunfe@gmail.com>
2026-08-06 19:10:07 +0100

    Time: 1 ms (100%), Space: 42.8 MB (14.66%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.java
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.java
@@ -1,8 +1,6 @@
 class Solution {
     public int smallestNumber(int n, int t) {
         while (n > 0) {
-            // int product = ;
-            // System.out.println("n = " + n + "; product=" + product);
             if (productOf(n) % t == 0) return n;
             n++;
         }
@@ -12,9 +10,8 @@ class Solution {
     private int productOf(int n) {
         int product = 1;
         while (n > 0) {
-            int next = n % 10;
+            product *= n % 10;
             n /= 10;
-            product *= next;
         }
         return product;
     }
