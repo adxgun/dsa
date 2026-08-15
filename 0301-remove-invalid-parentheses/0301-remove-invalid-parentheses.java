f1b5ae7824fcdd2302a1c405c255d7e209a3ef14
Hammed Adigun <adigunadunfe@gmail.com>
2026-08-15 18:16:08 +0100

    Time: 723 ms (5.04%), Space: 47.9 MB (32.98%) - LeetHub

---
+++ 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.java
@@ -0,0 +1,53 @@
+class Solution {
+    public List<String> removeInvalidParentheses(String s) {
+        List<String> result = new ArrayList<>();
+        Set<String> visited = new HashSet<>();
+        Queue<String> queue = new ArrayDeque<>();
+        queue.offer(s);
+        visited.add(s);
+
+        boolean found = false;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                String cur = queue.poll();
+
+                if (isValid(cur)) {
+                    result.add(cur);
+                    found = true;
+                }
+
+                if (found) continue;
+
+                for (int j = 0; j < cur.length(); j++) {
+                    char ch = cur.charAt(j);
+                    if (ch != '(' && ch != ')') continue;
+
+                    String nei = cur.substring(0, j) + cur.substring(j + 1);
+                    System.out.println(nei);
+                    if (!visited.contains(nei)) {
+                        visited.add(nei);
+                        queue.offer(nei);
+                    }
+                }
+                System.out.println();
+                System.out.println();
+            }
+
+            if (found) break;
+        }
+        return result;
+    }
+
+    private boolean isValid(String s) {
+        int balance = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') balance += 1;
+            else if (c == ')') {
+                balance -= 1;
+                if (balance < 0) return false;
+            }
+        }
+        return balance == 0;
+    }
+}
