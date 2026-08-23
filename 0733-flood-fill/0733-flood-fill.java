ffc42aecac9d926d32b9954d0f56c0daf2623fa5
Hammed Adigun <adigunadunfe@gmail.com>
2026-08-23 20:30:14 +0100

    Time: 1 ms (39%), Space: 46.7 MB (79.56%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -5,10 +5,8 @@ class Solution {
         int startColor = image[sr][sc];
         int rows = image.length;
         int cols = image[0].length;
-        boolean[][] visited = new boolean[rows][cols];
         Queue<int[]> queue = new ArrayDeque<>();
         queue.offer(new int[]{sr, sc});
-        visited[sr][sc] = true;
         image[sr][sc] = color;
 
         int[][] directions = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
@@ -21,10 +19,9 @@ class Solution {
 
                 if (nr >= 0 && nr < rows && 
                     nc >= 0 && nc < cols && 
-                    !visited[nr][nc] && image[nr][nc] == startColor) {
+                    image[nr][nc] == startColor) {
                         image[nr][nc] = color;
                         queue.offer(new int[]{nr, nc});
-                        visited[nr][nc] = true;
                 }
             }
         }
