86cb39241e9dee6e043332ff821d8184b2c310b6
Hammed Adigun <adigunadunfe@gmail.com>
2026-09-06 17:39:02 +0100

    Time: 9 ms (14.8%), Space: 45 MB (79.02%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,38 +1,43 @@
 
 class Solution {
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> result = new ArrayList<>();
+    private List<List<Integer>> result = new ArrayList<>();
+    private List<Integer> track = new ArrayList<>();
+    private int sum = 0;
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {    
+        if (candidates.length == 0) return result;
         
         Arrays.sort(candidates);
-        backtrack(candidates, target, 0, new ArrayList<>(), result);
+        backtrack(candidates, target, 0);
         return result;
     }
 
-    private void backtrack(int[] candidates, int target, int start, List<Integer> combs, List<List<Integer>> result) {
+    private void backtrack(
+        int[] candidates, 
+        int target, 
+        int start) {
 
-        if (target == 0) {
-            result.add(new ArrayList<>(combs));
+        if (sum == target) {
+            result.add(new ArrayList<>(track));
             return;
         }
 
-        if (target < 0) {
+        if (sum > target) {
             return;
         }
 
         for (int i = start; i < candidates.length; i++) {
             int candidate = candidates[i];
-
             if (i > start && candidates[i] == candidates[i - 1]) {
                 continue;
             }
 
-            if (candidate > target) {
-                break;
-            }
-
-            combs.add(candidate);
-            backtrack(candidates, target - candidate, i + 1, combs, result);
-            combs.remove(combs.size() - 1);
+            track.addLast(candidate);
+            sum += candidate;
+            
+            backtrack(candidates, target, i + 1);
+            
+            track.removeLast();
+            sum -= candidate;
         }
     }
 }
