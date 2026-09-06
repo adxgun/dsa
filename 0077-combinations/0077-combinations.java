b5cd93fb2fde045b407fcba691af0baf85dc66ef
Hammed Adigun <adigunadunfe@gmail.com>
2026-09-06 17:10:41 +0100

    Time: 16 ms (88.04%), Space: 97.9 MB (82.03%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -8,7 +8,10 @@ class Solution {
     }
 
     private void backtrack(int n, int k, int start) {
-        if (track.size() == k) result.add(new ArrayList<>(track));
+        if (track.size() == k) {
+            result.add(new ArrayList<>(track));
+            return;
+        }
 
         for (int i = start; i <= n; i++) {
             track.addLast(i);
