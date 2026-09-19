36b7f3507b8b6c88b0144355bfd06d1d7f8f803a
Hammed Adigun <adigunadunfe@gmail.com>
2026-09-19 20:38:47 +0100

    Time: 16 ms (16.08%), Space: 44.2 MB (44.18%) - LeetHub

---
+++ 0481-magical-string/0481-magical-string.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int magicalString(int n) {
+        if (n == 1 || n == 2) return 1;
+
+        int count = 1, startIndex = 2;
+        StringBuilder sb = new StringBuilder("122");
+        char toappend = '1'; // 3rd char is 2 so we append two occurence of '1'
+
+        for (int i = 0; i < n; i++) {
+            char nextChar = sb.charAt(startIndex);
+            int nextCount = nextChar - '0';
+            for (int j = 0; j < nextCount; j++) {
+                if (n == sb.length()) break;
+                sb.append(toappend);
+
+                if (toappend == '1') count += 1;
+            }
+
+            if (n == sb.length()) break;
+            toappend = (toappend == '2') ? '1' : '2';
+            startIndex += 1;
+        }
+        System.out.println(sb.toString());
+        return count;
+    }
+}
