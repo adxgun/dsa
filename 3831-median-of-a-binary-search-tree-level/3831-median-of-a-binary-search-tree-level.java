5d539ada9e54d46e114099ed26d3f8f909eb0d84
Hammed Adigun <adigunadunfe@gmail.com>
2026-09-22 00:01:39 +0100

    Time: 33 ms (6.67%), Space: 202.7 MB (6.67%) - LeetHub

--- 3831-median-of-a-binary-search-tree-level/3831-median-of-a-binary-search-tree-level.java
+++ 3831-median-of-a-binary-search-tree-level/3831-median-of-a-binary-search-tree-level.java
@@ -17,18 +17,19 @@ class Solution {
     public int levelMedian(TreeNode root, int level) {
         if (root == null) return 0;
         
-        Map<Integer, List<Integer>> levels = new HashMap<>();
-        dfs(root, levels, 0);
-        List<Integer> values = levels.get(level);
-        if (values == null || values.size() == 0) return -1;
-        int size = values.size();
-        return values.get(size / 2);
+        List<Integer> levels = new ArrayList<>();
+        dfs(root, levels, level, 0);
+        if (levels.size() == 0) return -1;
+        int size = levels.size();
+        return levels.get(size / 2);
     }
 
-    private void dfs(TreeNode root, Map<Integer, List<Integer>> levels, int depth) {
+    private void dfs(TreeNode root, List<Integer> levels, int level, int depth) {
         if (root == null) return;
-        dfs(root.left, levels, depth + 1);
-        levels.computeIfAbsent(depth, (k) -> new ArrayList<>()).add(root.val);
-        dfs(root.right, levels, depth + 1);
+        dfs(root.left, levels, level, depth + 1);
+        if (depth == level) {
+            levels.add(root.val);
+        }
+        dfs(root.right, levels, level, depth + 1);
     }
 }
